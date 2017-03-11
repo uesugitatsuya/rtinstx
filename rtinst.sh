31adcd0deeb431811a702286f6906fdb2174a8aa
uesugitatsuya <uesugitatsuya@users.noreply.github.com>
2017-03-11 15:10:05 +0800

    Update rtinst.sh

--- rtinst.sh
+++ rtinst.sh
@@ -17,7 +17,7 @@ libtorrentloc='http://rtorrent.net/downloads/libtorrent-'$libtorrentrel'.tar.gz'
 xmlrpcloc='https://svn.code.sf.net/p/xmlrpc-c/code/stable'
 
 blob=master
-rtdir=https://raw.githubusercontent.com/arakasi72/rtinst/$blob/scripts
+rtdir=https://raw.githubusercontent.com/uesugitatsuya/rtinstx/$blob/scripts
 
 if [ $(dpkg-query -W -f='${Status}' lsb-release 2>/dev/null | grep -c "ok installed") -gt 0 ]; then
   fullrel=$(lsb_release -sd)
